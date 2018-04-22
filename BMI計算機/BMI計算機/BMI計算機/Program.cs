f1256982d5d47b182ca0aa0394a121ee09c34dc7
DESKTOP-V6VH3CI\USER <shan880515@gmail.com>
2018-04-22 11:19:51 +0800

    modify

--- BMI計算機/BMI計算機/BMI計算機/Program.cs
+++ BMI計算機/BMI計算機/BMI計算機/Program.cs
@@ -10,34 +10,85 @@ namespace BMI計算機
     {
         static void Main(string[] args)
         {
+            //設 BMI計算機為標題
+            Console.Title = "BMI計算機";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("╔═══════════╗");
+            Console.WriteLine("║ BMI計算機 ║");
+            Console.WriteLine("╚═══════════╝");
+
+            //輸入身高體重
             Console.ForegroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write("輸入體重:");
+            Console.Write("輸入體重:");           
+
+            //轉型方式
             double kg = double.Parse(Console.ReadLine());
             Console.Write("輸入身高:");
             double cm = double.Parse(Console.ReadLine());
+
+            //計算BMI值
             double BMI = kg / ((cm * 0.01) * (cm * 0.01));
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("BMI值為:" + BMI);
 
+            //判斷BMI的範圍
             if (BMI < 16.5)
             {
-                Console.WriteLine("體重過輕，免役");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("╔════════════════════════╗");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("║  體重過輕，免疫        ║");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("╚════════════════════════╝");
+
+                //嗶嗶聲
+                Console.Beep(523, 100);
+                Console.Beep(587, 100);
+                Console.Beep(988, 100);
+                Console.Beep(880, 100);
             }
 
             if (BMI >= 16.5 && BMI < 26)
             {
-                Console.WriteLine("正常範圍，給我當兵");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("╔════════════════════════╗");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("║  正常範圍，給我當兵    ║");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("╚════════════════════════╝");
+
+                Console.Beep(784, 100);
+                Console.Beep(659, 100);
+                Console.Beep(784, 100);
+                Console.Beep(784, 100);
             }
 
             if (BMI >= 26 && BMI < 30)
             {
-                Console.WriteLine("過重，去減肥吧不用當兵");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("╔════════════════════════╗");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("║ 過重，去減肥吧!不用當兵║");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("╚════════════════════════╝");
+
+                Console.Beep(523, 100);
+                Console.Beep(659, 100);
+                Console.Beep(523, 100);
+                Console.Beep(587, 100);
             }
 
 
             if (BMI > 30)
             {
-                Console.WriteLine("超重，免役");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("╔════════════════════════╗");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("║  超重，免役            ║");
+                Console.WriteLine("║                        ║");
+                Console.WriteLine("╚════════════════════════╝");
+
                 Console.Beep(523, 100);
                 Console.Beep(587, 100);
                 Console.Beep(659, 100);
